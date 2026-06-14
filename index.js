8ab3d432f15bff235da14c4232d7c92233fa7401
Edwin E. Cazares Flores <ReyZafir@gmail.com>
2026-06-13 22:10:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/ 
+
+const convertBtn = document.getElementById("convert-btn")
+const inputNum = document.getElementById("input-num")
+let plength = document.getElementById("plength")
+let pvolume = document.getElementById("pvolume")
+let pmass = document.getElementById("pmass")
+
+
+convertBtn.addEventListener("click", function() {
+    conversion(inputNum.value)
+})
+
+function conversion(inp) {
+    inp = Number(inp)
+    plength.innerText = `
+        ${inp} meters = ${(inp * 3.281).toFixed(3)} feet | ${inp} feet = ${(inp / 3.281).toFixed(3)} meters
+    `
+    pvolume.innerText = `
+    ${inp} liters = ${(inp * 0.264).toFixed(3)} gallons | ${inp} gallons = ${(inp / 0.264).toFixed(3)} liters`
+    pmass.innerText = `
+    ${inp} kilos = ${(inp * 2.204).toFixed(3)} pounds | ${inp} pounds = ${(inp / 2.204).toFixed(3)} kilos`
+}
